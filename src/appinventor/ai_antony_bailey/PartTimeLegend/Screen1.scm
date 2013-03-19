31afe8393a3e62a2f8202fd5a6a66d96b9dcb0c3
Antony <Bailey>
2013-03-19 22:51:01 +0000

    Init

---
+++ src/appinventor/ai_antony_bailey/PartTimeLegend/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"YaVersion":"62","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"8","Uuid":"0","Icon":"antony-bailey.jpg","Title":"PartTimeLegend","$Components":[{"$Name":"asWeb","$Type":"ActivityStarter","$Version":"3","Uuid":"-1354586264","Action":"android.intent.action.VIEW"}]}}
+|#
